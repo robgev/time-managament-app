e1206598bb7009c0ad1209e33903afb7b50ecfc1
Robert Gevorgyan <robert1999.g@gmail.com>
2021-01-22 16:52:59 +0400

    Complete CRUD for entries

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,16 +1,46 @@
 import express, { Response } from 'express';
-import { getManager, Between } from 'typeorm';
+import { getManager } from 'typeorm';
 import { Task } from '../entity/Task';
+import { UserRole } from '../entity/User';
 import { RequestWithUser } from '../types/request.d';
 import authenticateToken from '../middlewares/authenticateToken';
 
 const router = express.Router();
 
 router.post('/createEntry', authenticateToken, async (req: RequestWithUser, res: Response) => {
+  const { body: taskData, user } = req;
+  if (user.id === taskData.byUser || user.role === UserRole.ADMIN) {
+    const taskRepository = getManager().getRepository(Task);
+    const task = taskRepository.create(taskData);
+    await taskRepository.save(task);
+    res.json(task);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+router.patch('/editEntry/:id', authenticateToken, async (req: RequestWithUser, res: Response) => {
+  const { params: { id }, body: updatedTask, user } = req;
   const taskRepository = getManager().getRepository(Task);
-  const task = taskRepository.create(req.body);
-  await taskRepository.save(task);
-  res.json(task);
+  const task = await taskRepository.findOne(id, { relations: ['byUser'] });
+  if (task.byUser.id === user.id || user.role === UserRole.ADMIN) {
+    await taskRepository.update(id, updatedTask);
+    res.json({ response: 'Updated successfully' });
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+router.delete('/deleteEntry/:id', authenticateToken, async (req: RequestWithUser, res: Response) => {
+  const { params: { id }, user } = req;
+  const taskRepository = getManager().getRepository(Task);
+  const task = await taskRepository.findOne(id, { relations: ['byUser'] });
+  if (task.byUser.id === user.id || user.role === UserRole.ADMIN) {
+    await taskRepository.delete(id);
+    res.json({ response: 'Deleted successfully' });
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 router.get('/entries', authenticateToken, async (req: RequestWithUser, res: Response) => {
