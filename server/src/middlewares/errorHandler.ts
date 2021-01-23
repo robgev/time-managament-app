d31d366a3d7e95f0d478063f951c6ccbfc15e1d1
Robert Gevorgyan <robert1999.g@gmail.com>
2021-01-23 23:05:13 +0400

    Add error handling middleware

---
+++ server/src/middlewares/errorHandler.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { SyntheticError } from '../types/SyntheticError';
+
+// Wont use next, error handler will come the last
+export default (err: SyntheticError, req: Request, res: Response) => {
+  if (err.message) {
+    const { message, errors, code } = err;
+    res
+      .status(err.status)
+      .json({
+        code,
+        message,
+        errors,
+      });
+  } else {
+    // All non-synthetic errors
+    console.error(err); // For debugging purposes
+    res.status(500).json({
+      code: 'Unknown',
+      message: 'Something went wrong...',
+      error: err,
+    });
+  }
+};
